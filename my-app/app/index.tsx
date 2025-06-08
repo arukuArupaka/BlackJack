ba68bf2e571af665b39bbc47196ea4215d548eb7
shiomi4030918 <shiomiaki0918403@gmail.com>
2025-06-08 15:53:32 +0900

    ボタンを押したら数字増える

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
-
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { useState } from "react";
 export default function Index() {
+  const [number, setNumber] = useState(0);
+  function numberIncrease(buttonNumber: number) {
+    buttonNumber += 1;
+    return buttonNumber;
+  } 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <TouchableOpacity onPress={() => setNumber(numberIncrease(number))} style={{ height:100, width:300, backgroundColor:"red", justifyContent:"center", alignItems:"center"}}>
+        <Text style={{ color:"white", fontSize:20, fontWeight:"bold"}}>ボタン</Text>
+      </TouchableOpacity>
+      <View>
+        <Text style={{ fontSize: 60, fontWeight: "bold", paddingTop: 20}}>{number}</Text>
+      </View>
     </View>
   );
 }

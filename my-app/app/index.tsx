a2ac470bffa774d3bb70e27390078698c0d335cb
shiomi4030918 <shiomiaki0918403@gmail.com>
2025-06-08 17:24:41 +0900

    背景画像追加

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,6 +1,7 @@
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, ImageBackground } from "react-native";
 import { useState } from "react";
 import { isSearchBarAvailableForCurrentPlatform } from "react-native-screens";
+
 export default function Index() {
   const [number, setNumber] = useState(0);
   function numberIncrease(buttonNumber: number) {
@@ -9,25 +10,30 @@ export default function Index() {
   } 
   function numberDecrease(buttonNumber: number) {
     if (number !=0){
-     buttonNumber -= 1;  
+      buttonNumber -= 1;  
     }else{
       return buttonNumber; 
     }
     return buttonNumber;
   }
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <TouchableOpacity onPress={() => setNumber(numberIncrease(number))} style={{ height:100, width:300, backgroundColor:"red", justifyContent:"center", alignItems:"center"}}>
-        <Text style={{ color:"white", fontSize:20, fontWeight:"bold"}}>ボタン</Text>
-      </TouchableOpacity>
-      <View>
-        <Text style={{ fontSize: 60, fontWeight: "bold", paddingTop: 20, color: (number %11 === 0 && number !== 0) ? "red" : "white"}}>{number}</Text>
-      </View>
-      <View>
-        <TouchableOpacity onPress={() =>setNumber(numberDecrease(number))} style={{height:100, width:300, backgroundColor:"blue", justifyContent:"center", alignItems:"center" }}>
-          <Text style={{ color:"white", fontSize:20, fontWeight: "bold", }}>ボタン</Text>
+    <ImageBackground 
+      source={require('../image/7c45c5c8-06b6-4ef3-a46b-46e6ac72c2cd.jpg')}
+      style={{ flex: 1 }}
+    >
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <TouchableOpacity onPress={() => setNumber(numberIncrease(number))} style={{ height:100, width:300, backgroundColor:"red", justifyContent:"center", alignItems:"center"}}>
+          <Text style={{ color:"white", fontSize:20, fontWeight:"bold"}}>ボタン</Text>
         </TouchableOpacity>
+        <View>
+          <Text style={{ fontSize: 60, fontWeight: "bold", paddingTop: 20, color: (number %11 === 0 && number !== 0) ? "red" : "white"}}>{number}</Text>
+        </View>
+        <View>
+          <TouchableOpacity onPress={() =>setNumber(numberDecrease(number))} style={{height:100, width:300, backgroundColor:"blue", justifyContent:"center", alignItems:"center" }}>
+            <Text style={{ color:"white", fontSize:20, fontWeight: "bold", }}>ボタン</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 }

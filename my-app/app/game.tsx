ef1c50f2b8dcdd23592a8d817a21850831e2bc1c
koki sawada <sawadakoki040903@gmail.com>
2025-06-11 18:40:23 +0900

    最新（プレイ画面）

--- my-app/app/game.tsx
+++ my-app/app/game.tsx
@@ -1,13 +1,67 @@
 import { router } from "expo-router";
 import { useState } from "react";
 import { Image, ImageBackground, Text, TouchableOpacity, View } from "react-native";
+ const cardImages: Record<string, any> = { //ここなに？？？
+  "01c": require("../cards/01c.gif"),
+  "02c": require("../cards/02c.gif"),
+  "03c": require("../cards/03c.gif"),
+  "04c": require("../cards/04c.gif"),
+  "05c": require("../cards/05c.gif"),
+  "06c": require("../cards/06c.gif"),
+  "07c": require("../cards/07c.gif"),
+  "08c": require("../cards/08c.gif"),
+  "09c": require("../cards/09c.gif"),
+  "10c": require("../cards/10c.gif"),
+  "11c": require("../cards/11c.gif"),
+  "12c": require("../cards/12c.gif"),
+  "13c": require("../cards/13c.gif"),
+  "01d": require("../cards/01d.gif"),
+  "02d": require("../cards/02d.gif"),
+  "03d": require("../cards/03d.gif"),
+  "04d": require("../cards/04d.gif"),
+  "05d": require("../cards/05d.gif"),
+  "06d": require("../cards/06d.gif"),
+  "07d": require("../cards/07d.gif"),
+  "08d": require("../cards/08d.gif"),
+  "09d": require("../cards/09d.gif"),
+  "10d": require("../cards/10d.gif"),
+  "11d": require("../cards/11d.gif"),
+  "12d": require("../cards/12d.gif"),
+  "13d": require("../cards/13d.gif"),
+  "01h": require("../cards/01h.gif"),
+  "02h": require("../cards/02h.gif"),
+  "03h": require("../cards/03h.gif"),
+  "04h": require("../cards/04h.gif"),
+  "05h": require("../cards/05h.gif"),
+  "06h": require("../cards/06h.gif"),
+  "07h": require("../cards/07h.gif"),
+  "08h": require("../cards/08h.gif"),
+  "09h": require("../cards/09h.gif"),
+  "10h": require("../cards/10h.gif"),
+  "11h": require("../cards/11h.gif"),
+  "12h": require("../cards/12h.gif"),
+  "13h": require("../cards/13h.gif"),
+  "01s": require("../cards/01s.gif"),
+  "02s": require("../cards/02s.gif"),
+  "03s": require("../cards/03s.gif"),
+  "04s": require("../cards/04s.gif"),
+  "05s": require("../cards/05s.gif"),
+  "06s": require("../cards/06s.gif"),
+  "07s": require("../cards/07s.gif"),
+  "08s": require("../cards/08s.gif"),
+  "09s": require("../cards/09s.gif"),
+  "10s": require("../cards/10s.gif"),
+  "11s": require("../cards/11s.gif"),
+  "12s": require("../cards/12s.gif"),
+  "13s": require("../cards/13s.gif"),
+};
 export default function Game() {
  
   const [cards, setCards] = useState<string[]>([]);
   const [cards2, setCards2] = useState<string[]>([]);
   const mark = ["c", "d", "h", "s"];
   const num = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13"];
-  const deck = mark.flatMap(mark => num.map(num => `${mark}${num}`));
+  const deck = num.flatMap(num => mark.map(mark => `${num}${mark}`));
   
   const drawtwocards = () =>{
   const shuffle = [...deck].sort(() => Math.random() - 0.5);//シャッフル
@@ -19,9 +73,7 @@ export default function Game() {
   const draw = shuffle.slice(0, 2);//二枚ドロー
   setCards2(draw) ;
 };
-  //const cardimg = (cards) =>{
-    //return require(`../assets/cards/${cards}.gif`)
-  //}
+
 return(
     <ImageBackground 
           source={require('../image/7c45c5c8-06b6-4ef3-a46b-46e6ac72c2cd.jpg')}
@@ -29,15 +81,21 @@ return(
         >
           <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
  <TouchableOpacity onPress={drawtwocards} style= {{height:98, width:77, backgroundColor:"rgba(217, 217, 217, 0.47)", justifyContent:"center", alignItems:"center",borderRadius:15}}>
-              <Text>{cards}</Text>
-
+              <Text>{cards[0]}</Text>
+              <Text style={{ position: 'absolute', bottom: 10, right: 10 }}>{cards[1]}</Text>
+                 
+                  {cards.map((card) => ( //ここなに？？？この指定をしないとカードは表示できない？？できない？？
+            <Image
+              key={card} // カード名をkeyに
+              source={cardImages[card]}
+            />))}
 
             </TouchableOpacity>
 
 
             <TouchableOpacity onPress={drawtwocards2} style= {{height:98, width:77, backgroundColor:"rgba(217, 217, 217, 0.47)", justifyContent:"center", alignItems:"center",borderRadius:15, marginTop:150}}>
               <Text>{cards2}</Text>
-
+              
 
             </TouchableOpacity>
 

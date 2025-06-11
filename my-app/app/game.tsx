4e739fdf09f3a0b1a8ea982add7467ae04ecf78e
koki sawada <sawadakoki040903@gmail.com>
2025-06-11 17:48:23 +0900

    プレイ画面レイアウト

--- my-app/app/game.tsx
+++ my-app/app/game.tsx
@@ -4,6 +4,7 @@ import { Image, ImageBackground, Text, TouchableOpacity, View } from "react-nati
 export default function Game() {
  
   const [cards, setCards] = useState<string[]>([]);
+  const [cards2, setCards2] = useState<string[]>([]);
   const mark = ["c", "d", "h", "s"];
   const num = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13"];
   const deck = mark.flatMap(mark => num.map(num => `${mark}${num}`));
@@ -12,6 +13,11 @@ export default function Game() {
   const shuffle = [...deck].sort(() => Math.random() - 0.5);//シャッフル
   const draw = shuffle.slice(0, 2);//二枚ドロー
   setCards(draw) ;
+  }
+  const drawtwocards2 = () =>{
+  const shuffle = [...deck].sort(() => Math.random() - 0.5);//シャッフル
+  const draw = shuffle.slice(0, 2);//二枚ドロー
+  setCards2(draw) ;
 };
   //const cardimg = (cards) =>{
     //return require(`../assets/cards/${cards}.gif`)
@@ -22,21 +28,27 @@ return(
           style={{ flex: 1 }}
         >
           <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-
-            <TouchableOpacity onPress={drawtwocards} style= {{height:100, width:200, backgroundColor:"red", justifyContent:"center", alignItems:"center"}}>
+ <TouchableOpacity onPress={drawtwocards} style= {{height:98, width:77, backgroundColor:"rgba(217, 217, 217, 0.47)", justifyContent:"center", alignItems:"center",borderRadius:15}}>
               <Text>{cards}</Text>
 
 
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => router.push("/result")} style={{width:171.5,height:85.5,backgroundColor:"#DDDDDD",justifyContent: "center", alignItems: "center",marginVertical:15,borderRadius:30,marginTop:50}}>
+
+            <TouchableOpacity onPress={drawtwocards2} style= {{height:98, width:77, backgroundColor:"rgba(217, 217, 217, 0.47)", justifyContent:"center", alignItems:"center",borderRadius:15, marginTop:150}}>
+              <Text>{cards2}</Text>
+
+
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => router.push("/result")} style={{width:171.5,height:85.5,backgroundColor:"#DDDDDD",justifyContent: "center", alignItems: "center",marginVertical:15,borderRadius:30,marginTop:100}}>
            <Image
           source={require('../image/start.jpg')}
           style={{width:171.5,height:85.5}}>
             </Image>
 
         </TouchableOpacity>
-    <View style={{ flexDirection: 'row',justifyContent: 'space-between', marginTop: 0  }}>     <TouchableOpacity  style={{width:171.5,height:85.5,backgroundColor:"#DDDDDD",justifyContent: "center", alignItems: "center",marginVertical:15,borderRadius:30,marginTop:20,marginRight:20}}>
+    <View style={{ flexDirection: 'row',justifyContent: 'space-between' }}>     <TouchableOpacity  style={{width:171.5,height:85.5,backgroundColor:"#DDDDDD",justifyContent: "center", alignItems: "center",marginVertical:15,borderRadius:30,marginTop:20,marginRight:20}}>
            <Image
           source={require('../image/start.jpg')}
           style={{width:171.5,height:85.5, }}>

49dbaaff0bacdf376c1a366f7b285af50bbc8e6e
koki sawada <sawadakoki040903@gmail.com>
2025-06-11 16:46:13 +0900

    Hit,Standの位置決め

--- my-app/app/game.tsx
+++ my-app/app/game.tsx
@@ -1,61 +1,8 @@
-import { useState } from "react";
 import { router } from "expo-router";
-import { Image, ImageBackground, TouchableOpacity, View, Text } from "react-native";
+import { useState } from "react";
+import { Image, ImageBackground, Text, TouchableOpacity, View } from "react-native";
 export default function Game() {
-  const cardImages = {
-  c01: require("../assets/cards/c01.gif"),
-  c02: require("../assets/cards/c02.gif"),
-  c03: require("../assets/cards/c03.gif"),
-  c04: require("../assets/cards/c04.gif"),
-  c05: require("../assets/cards/c05.gif"),
-  c06: require("../assets/cards/c06.gif"),
-  c07: require("../assets/cards/c07.gif"),
-  c08: require("../assets/cards/c08.gif"),
-  c09: require("../assets/cards/c09.gif"),
-  c10: require("../assets/cards/c10.gif"),
-  c11: require("../assets/cards/c11.gif"),
-  c12: require("../assets/cards/c12.gif"),
-  c13: require("../assets/cards/c13.gif"),
-  d01: require("../assets/cards/d01.gif"),
-  d02: require("../assets/cards/d02.gif"),
-  d03: require("../assets/cards/d03.gif"),
-  d04: require("../assets/cards/d04.gif"),
-  d05: require("../assets/cards/d05.gif"),
-  d06: require("../assets/cards/d06.gif"),
-  d07: require("../assets/cards/d07.gif"),
-  d08: require("../assets/cards/d08.gif"),
-  d09: require("../assets/cards/d09.gif"),
-  d10: require("../assets/cards/d10.gif"),
-  d11: require("../assets/cards/d11.gif"),
-  d12: require("../assets/cards/d12.gif"),
-  d13: require("../assets/cards/d13.gif"),
-  h01: require("../assets/cards/h01.gif"),
-  h02: require("../assets/cards/h02.gif"),
-  h03: require("../assets/cards/h03.gif"),
-  h04: require("../assets/cards/h04.gif"),
-  h05: require("../assets/cards/h05.gif"),
-  h06: require("../assets/cards/h06.gif"),
-  h07: require("../assets/cards/h07.gif"),
-  h08: require("../assets/cards/h08.gif"),
-  h09: require("../assets/cards/h09.gif"),
-  h10: require("../assets/cards/h10.gif"),
-  h11: require("../assets/cards/h11.gif"),
-  h12: require("../assets/cards/h12.gif"),
-  h13: require("../assets/cards/h13.gif"),
-  s01: require("../assets/cards/s01.gif"),
-  s02: require("../assets/cards/s02.gif"),
-  s03: require("../assets/cards/s03.gif"),
-  s04: require("../assets/cards/s04.gif"),
-  s05: require("../assets/cards/s05.gif"),
-  s06: require("../assets/cards/s06.gif"),
-  s07: require("../assets/cards/s07.gif"),
-  s08: require("../assets/cards/s08.gif"),
-  s09: require("../assets/cards/s09.gif"),
-  s10: require("../assets/cards/s10.gif"),
-  s11: require("../assets/cards/s11.gif"),
-  s12: require("../assets/cards/s12.gif"),
-  s13: require("../assets/cards/s13.gif"),
-};
+ 
   const [cards, setCards] = useState<string[]>([]);
   const mark = ["c", "d", "h", "s"];
   const num = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13"];
@@ -80,16 +27,30 @@ return(
 
 
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => router.back()} style={{width:300,height:150,backgroundColor:"#D9D9D9",justifyContent: "center", alignItems: "center",marginVertical:15,borderRadius:30,marginTop:50}}>
+
+            <TouchableOpacity onPress={() => router.back()} style={{width:171.5,height:85.5,backgroundColor:"#DDDDDD",justifyContent: "center", alignItems: "center",marginVertical:15,borderRadius:30,marginTop:50}}>
            <Image
           source={require('../image/start.jpg')}
-          style={{width:220,height:50}}>
+          style={{width:171.5,height:85.5}}>
             </Image>
 
         </TouchableOpacity>
-    
-  
-          </View>
+    <View style={{ flexDirection: 'row',justifyContent: 'space-between', marginTop: 0  }}>     <TouchableOpacity  style={{width:171.5,height:85.5,backgroundColor:"#DDDDDD",justifyContent: "center", alignItems: "center",marginVertical:15,borderRadius:30,marginTop:20,marginRight:20}}>
+           <Image
+          source={require('../image/start.jpg')}
+          style={{width:171.5,height:85.5, }}>
+            </Image>
+
+        </TouchableOpacity>
+    <TouchableOpacity  style={{width:171.5,height:85.5,backgroundColor:"#DDDDDD",justifyContent: "center", alignItems: "center",marginVertical:15,borderRadius:30,marginTop:20}}>
+           <Image
+          source={require('../image/start.jpg')}
+          style={{width:171.5,height:85.5, }}>
+            </Image>
+
+        </TouchableOpacity></View>
+  </View>
+        
         </ImageBackground>
 )
 }
